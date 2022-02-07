4b18bcae54622c9174466629ba47d842c34ea5b5
Geena Jackson <gjacks303@gmail.com>
2022-02-07 15:51:00 -0700

    Create average step goal method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -8,6 +8,12 @@ class UserRepository {
         return this.currentUser = (this.userData.find(user => user.id === id));
     }
 
+    averageStepGoal() {
+      let average = this.userData.reduce((acc, user) => {
+        return acc += user.dailyStepGoal / this.userData.length
+      }, 0)
+      return average
+    }
 }
 
 export default UserRepository;
