e71d53bf83641cec1eb9a608635d611ba1adb8b5
Geena Jackson <gjacks303@gmail.com>
2022-02-07 16:53:36 -0700

    Add user class

--- src/user.js
+++ src/user.js
@@ -1,9 +1,19 @@
 class User {
-    constructor() {
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.name;
+    this.address = user.address;
+    this.email = user.email;
+    this.strideLength = user.strideLength;
+    this.dailyStepGoal = user.dailyStepGoal;
+    this.friends = user.friends;
+  }
 
-    }
+  returnFirstName() {
+    return this.name.split(" ")[0]
+  }
 
-    
+
 };
 
 export default User
