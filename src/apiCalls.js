3558224fbbc82189940d99783ea159e80203a03d
Francesca McConnell <fmcconnell59@gmail.com>
2022-02-09 14:32:53 -0700

    Remove scripts.js script

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,11 @@
 // Your fetch requests will live here!
 
-// const urls = [
-//     'https://fitlit-api.herokuapp.com/api/v1/users',
-//     'https://fitlit-api.herokuapp.com/api/v1/sleep',
-//     'https://fitlit-api.herokuapp.com/api/v1/activity',
-//     'https://fitlit-api.herokuapp.com/api/v1/hydration'
-// ];
+const urls = [
+    'https://fitlit-api.herokuapp.com/api/v1/users',
+    'https://fitlit-api.herokuapp.com/api/v1/sleep',
+    'https://fitlit-api.herokuapp.com/api/v1/activity',
+    'https://fitlit-api.herokuapp.com/api/v1/hydration'
+];
 
 const getUsers = () => {
     return fetch('https://fitlit-api.herokuapp.com/api/v1/users')
