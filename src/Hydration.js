f207a695df165b75cd1056238499b79048279a53
Francesca McConnell <fmcconnell59@gmail.com>
2022-02-09 14:30:54 -0700

    Add calculate avg water

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,8 +1,16 @@
 class Hydration {
     constructor(hydration) {
         this.userID = hydration.userID;
-        this.date = hydration.date;
-        this.numOunces = hydration.numOunces;
+        this.data = hydration;
+        // this.dates = hydration.date;
+        // this.numOunces = hydration.numOunces;
+    }
+
+    calculateAvgWater() {
+        let waterAverage = this.data.reduce((acc, userLog) => {
+            return acc += userLog.numOunces / this.data.length;
+          }, 0);
+          return waterAverage;
     }
 }
 
