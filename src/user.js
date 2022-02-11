a8247e50693b647108c1c75c3d1be65d9281ed66
Geena Jackson <gjacks303@gmail.com>
2022-02-11 14:59:02 -0700

    Add methods to find dates

--- src/user.js
+++ src/user.js
@@ -7,11 +7,23 @@ class User {
     this.strideLength = user.strideLength;
     this.dailyStepGoal = user.dailyStepGoal;
     this.friends = user.friends;
+    this.userHydration = hydration.filter(data => data.userID === this.id);
+    this.date = ''
+    this.week = []
   };
 
   returnFirstName() {
     return this.name.split(" ")[0];
-  };
+  }
+
+  returnCurrentDate() {
+    return this.date = this.userHydration[this.userHydration.length - 1].date
+  }
+
+  returnLastWeek() {
+    return this.week = this.userHydration.map(entry => entry.date).reverse().filter((entry, index) => (index <= 6)).reverse()
+    // return this.week = this.userHydration.date.reverse().map(entry => this.week.push(entry.date))
+  }
 };
 
 export default User;
