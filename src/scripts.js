774645d26d7adf0d30daee15b7975b5d8ac0efad
Francesca McConnell <fmcconnell59@gmail.com>
2022-02-22 17:04:32 -0700

    Refactor innerHTML

--- src/scripts.js
+++ src/scripts.js
@@ -117,56 +117,54 @@ const displayUserInfo = user => {
   let friendFirstName = user.formattedFriends.map(friend => ' ' + friend.name.split(" ")[0]);
   return userInfo.innerHTML = `
   <h2>About you:</h2>
-  <p>${user.name}<br>
-  ${user.email}<br>
-  ${user.address}<br><br>
-  Your stride length:<br>
-  ${user.strideLength} feet<br><br>
-  Your daily step goal:<br>
-  ${user.dailyStepGoal}<br><br>
-  Your friends:<br>
-  ${friendFirstName}<br><br>
-  Your step goal compared to the average step goal amongst all users:<br>
-  ${user.dailyStepGoal}/${userRepo.averageStepGoal()}</p><br>
+    <p>${user.name}<br>
+    ${user.email}<br>
+    ${user.address}<br><br>
+    Your stride length:<br>
+    ${user.strideLength} feet<br><br>
+    Your daily step goal:<br>
+    ${user.dailyStepGoal}<br><br>
+    Your friends:<br>
+    ${friendFirstName}<br><br>
+    Your step goal compared to the average step goal amongst all users:<br>
+    ${user.dailyStepGoal}/${userRepo.averageStepGoal()}</p><br>
 `};
 
 const displayHydrationInfo = (user, hydration) => {
   return hydrationWidget.innerHTML = `
-
-  <p>
-    <img src="https://user-images.githubusercontent.com/88151743/153778199-6085b751-5f2d-45b8-b6f4-342bec5fbb5c.png"/>
-  </p>
-
+  <br>
+  <img src="https://user-images.githubusercontent.com/88151743/153778199-6085b751-5f2d-45b8-b6f4-342bec5fbb5c.png"/>
+  <br>
   <h2>Your Hydration Stats:</h2>
-  <h3> Oz of water today: ${hydration.calculateOuncesPerDayByDate()}</h3><br>
-  Oz of water on ${hydration.week[0].date}: ${hydration.week[0].numOunces}<br><br>
-  Oz of water on ${hydration.week[1].date}: ${hydration.week[1].numOunces}<br><br>
-  Oz of water on ${hydration.week[2].date}: ${hydration.week[2].numOunces}<br><br>
-  Oz of water on ${hydration.week[3].date}: ${hydration.week[3].numOunces}<br><br>
-  Oz of water on ${hydration.week[4].date}: ${hydration.week[4].numOunces}<br><br>
-  Oz of water on ${hydration.week[5].date}: ${hydration.week[5].numOunces}<br><br>
-  Oz of water on ${hydration.week[6].date}: ${hydration.week[6].numOunces}<br><br>
+    <h3> Water consumption today: ${hydration.calculateOuncesPerDayByDate()} oz</h3>
+      <p>${hydration.week[0].date}: ${hydration.week[0].numOunces} oz<br><br>
+      ${hydration.week[1].date}: ${hydration.week[1].numOunces} oz<br><br>
+      ${hydration.week[2].date}: ${hydration.week[2].numOunces} oz<br><br>
+      ${hydration.week[3].date}: ${hydration.week[3].numOunces} oz<br><br>
+      ${hydration.week[4].date}: ${hydration.week[4].numOunces} oz<br><br>
+      ${hydration.week[5].date}: ${hydration.week[5].numOunces} oz<br><br>
+      ${hydration.week[6].date}: ${hydration.week[6].numOunces} oz</p><br><br>
 `};
 
 const displaySleepInfo = sleep => {
   return sleepWidget.innerHTML = `
-  <p>
-    <img src="https://img.icons8.com/external-justicon-lineal-justicon/64/000000/external-sleep-emoji-justicon-lineal-justicon.png"/>
-  </p>
+  <br>
+  <img src="https://img.icons8.com/external-justicon-lineal-justicon/64/000000/external-sleep-emoji-justicon-lineal-justicon.png"/>
+  <br>
   <h2>Your Sleep Stats:</h2>
-  Today:<br>
-  Hours slept: ${sleep.hoursSleptPerDay('2020/01/20')} | Sleep quality: ${sleep.qualitySleptPerDay('2020/01/20')}<br><br>
-  Over last week:<br>
-  Average hours slept: ${sleep.calculateHrsSleptPerDayOverWeek('2019/06/15')} | Average sleep quality: ${sleep.calculateQualSleepPerDayOverWeek('2019/06/15')}<br><br>
-  All time:<br>
-  Average hours slept: ${sleep.userAverageHoursSleptPerDayTotal()} | Average sleep quality: ${sleep.userAverageQualitySleptPerDay()}
+    <h3>Today:</h3>
+      <p>Hours slept: ${sleep.hoursSleptPerDay('2020/01/20')} | Sleep quality: ${sleep.qualitySleptPerDay('2020/01/20')}<br>
+    <h3>Over last week:</h3>
+      <p>Average hours slept: ${sleep.calculateHrsSleptPerDayOverWeek('2019/06/15')} | Average sleep quality: ${sleep.calculateQualSleepPerDayOverWeek('2019/06/15')}<br>
+    <h3>All time:</h3>
+      <p>Average hours slept: ${sleep.userAverageHoursSleptPerDayTotal()} | Average sleep quality: ${sleep.userAverageQualitySleptPerDay()}</p>
 `};
 
 const displayActivityInfo = () => {
   return activityWidget.innerHTML = `
-    <p>
+  <br>
   <img src="https://img.icons8.com/ios/50/000000/walking--v1.png"/>
-    <p>
+  <br>
   <h2>Your Activity Data:</h2>
 `};
 
